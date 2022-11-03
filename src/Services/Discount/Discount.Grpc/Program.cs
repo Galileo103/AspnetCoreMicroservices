b1de5b116503696a9b5211911a5860e9af01f717
Galal Shaban <galal.shaban@gameball.co>
2022-11-03 02:36:42 +0200

    [Discount.Grpc] Developing CRUD in DiscountService class to Implement CRUD Grpc Proto Service

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,6 @@
 using Discount.Grpc.Extensions;
 using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 using Discount.Grpcpositories;
 
 
@@ -11,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
     // Add services to the container.
     builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+    //builder.Services.AddAutoMapper(typeof(Program));
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
     builder.Services.AddGrpc();
 }
 
@@ -19,7 +22,7 @@ var app = builder.Build();
     app.MigrateDatabase<Program>();
 
     // Configure the HTTP request pipeline.
-    //app.MapGrpcService<GreeterService>();
+    app.MapGrpcService<DiscountService>();
     app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
     app.Run();
